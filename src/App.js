f29cf0ad190b5edd41738127c04db888c1dc5e78
angie <angietorresbetancourt.1995@gmail.com>
2022-12-15 18:59:55 +0100

    Co-authored-by: Óscar Oyaga <Oscar-oy@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar'
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <AppBar position='static'>
+        <toolbar></toolbar>  
+      </AppBar>
+
+    </div>
+    /*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +25,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div>*/
   );
 }
 
